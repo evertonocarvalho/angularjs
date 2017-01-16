308e8c5b4f9541cde5279960f45c976a932fa201
Everton <evertonocarvalho@gmail.com>
2017-01-16 15:59:47 -0200

    Initial commit.

--- AngularJS/js/helloWorld.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author Everton Carvalho [evertonocarvalho@gmail.com]
- */
-
-var app = angular.module("helloWorld", []);
-app.controller("helloWorldController", function ($scope) {
-	$scope.message = "Hello World!";
-});
