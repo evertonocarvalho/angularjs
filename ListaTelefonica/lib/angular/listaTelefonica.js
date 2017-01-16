3739b1804f7a1dddbbf363d5e605e23fefa4b71f
Everton <evertonocarvalho@gmail.com>
2017-01-16 17:44:10 -0200

    Initial commit.

---
+++ ListaTelefonica/lib/angular/listaTelefonica.js
@@ -0,0 +1,8 @@
+/**
+ * @author Everton Carvalho [evertonocarvalho@gmail.com] 
+ */
+
+var app = angular.module("listaTelefonica", []);
+app.controller("listaTelefonicaController", function ($scope) {
+	$scope.app = "Lista Telefônica";
+});
