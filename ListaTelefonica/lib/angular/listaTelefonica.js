33b7f047e62884e3ec0bc190cc81264cca73cc03
Everton <evertonocarvalho@gmail.com>
2017-01-17 21:25:23 -0200

    Diretivas ngDisabled, ngOptions, ngClass, ngStyle, ngShow, ngHide, ngIf e ngInclude.

--- ListaTelefonica/lib/angular/listaTelefonica.js
+++ ListaTelefonica/lib/angular/listaTelefonica.js
@@ -7,13 +7,34 @@ app.controller("listaTelefonicaController", function ($scope) {
 	$scope.app = "Lista Telefônica";
 
 	$scope.contatos = [
-		{nome: "Pedro", telefone: "99998888"},
-		{nome: "Ana", telefone: "99998877"},
-		{nome: "Maria", telefone: "99998866"}
+		{nome: "Pedro", telefone: "99998888", cor: "blue"},
+		{nome: "Ana", telefone: "99998877", cor: "yellow"},
+		{nome: "Maria", telefone: "99998866", cor: "red"}
+	];
+
+	$scope.operadoras = [
+		{nome: "Oi", codigo: 14, categoria: "Celular"},
+		{nome: "VIVO", codigo: 15, categoria: "Celular"},
+		{nome: "TIM", codigo: 41, categoria: "Celular"},
+		{nome: "Claro", codigo: 21, categoria: "Celular"},
+		{nome: "GVT", codigo: 25, categoria: "Fixo"},
+		{nome: "Embratel", codigo: 21, categoria: "Fixo"}
 	];
 
 	$scope.adicionarContato = function (contato) {
 		$scope.contatos.push(angular.copy(contato));
 		delete $scope.contato;
+	};
+
+	$scope.apagarContatos = function (contatos) {
+		$scope.contatos = contatos.filter(function (contato) {
+			if (!contato.selecionado) return contato;
+		});
+	};
+
+	$scope.isContatoSelecionado = function (contatos) {
+		return contatos.some(function (contato) {
+			return contato.selecionado;
+		});
 	}
 });
